ea1cc4a42b71cc3083ac7c3d8fe308f06f31faa5
Howard.Zuo <leftstick@qq.com>
2019-05-29 16:41:43 +0800

    feat: adjust with vianet feature

--- src/controller/mail-window-controller.js
+++ src/controller/mail-window-controller.js
@@ -1,11 +1,9 @@
-const { BrowserWindow, shell, ipcMain } = require('electron')
+const { BrowserWindow, BrowserView, shell, ipcMain } = require('electron')
 const settings = require('electron-settings')
 const CssInjector = require('../js/css-injector')
 const path = require('path')
 
 const outlookUrl = 'https://partner.outlook.cn/owa/'
-const deeplinkUrls = ['outlook.live.com/mail/deeplink', 'outlook.office365.com/mail/deeplink', 'outlook.office.com/mail/deeplink']
-const outlookUrls = ['outlook.live.com', 'outlook.office365.com', 'outlook.office.com']
 
 class MailWindowController {
     constructor() {
@@ -25,7 +23,14 @@ class MailWindowController {
             frame: showWindowFrame,
             autoHideMenuBar: true,
             show: false,
-            icon: path.join(__dirname, '../../assets/outlook_linux_black.png')
+            icon: path.join(__dirname, '../../assets/outlook_linux_black.png'),
+            webPreferences: {
+                webSecurity: false,
+                nodeIntegration: true,
+                allowRunningInsecureContent: true,
+                nativeWindowOpen: true,
+                preload: path.join(__dirname, '../inject/setup.js')
+            }
         })
 
         // and load the index.html of the app.
@@ -36,12 +41,10 @@ class MailWindowController {
             this.show()
         })
 
-        // insert styles
+        //insert styles
         this.win.webContents.on('dom-ready', () => {
             this.win.webContents.insertCSS(CssInjector.main)
             if (!showWindowFrame) this.win.webContents.insertCSS(CssInjector.noFrame)
-
-            this.addUnreadNumberObserver()
 
             this.win.show()
         })
@@ -54,71 +57,6 @@ class MailWindowController {
             this.win = null
         })
 
-        // Open the new window in external browser
-        this.win.webContents.on('new-window', this.openInBrowser)
-    }
-
-    addUnreadNumberObserver() {
-        this.win.webContents.executeJavaScript(`
-            setTimeout(() => {
-                let unreadSpan = document.querySelector('._2iKri0mE1PM9vmRn--wKyI');
-                require('electron').ipcRenderer.send('updateUnread', unreadSpan.hasChildNodes());
-
-                let observer = new MutationObserver(mutations => {
-                    mutations.forEach(mutation => {
-                        console.log('Observer Changed.');
-                        require('electron').ipcRenderer.send('updateUnread', unreadSpan.hasChildNodes());
-
-                        // Scrape messages and pop up a notification
-                        var messages = document.querySelectorAll('div[role="listbox"][aria-label="Message list"]');
-                        if (messages.length)
-                        {
-                            var unread = messages[0].querySelectorAll('div[aria-label^="Unread"]');
-                            var body = "";
-                            for (var i = 0; i < unread.length; i++)
-                            {
-                                if (body.length)
-                                {
-                                    body += "\\n";
-                                }
-                                body += unread[i].getAttribute("aria-label").substring(7, 127);
-                            }
-                            if (unread.length)
-                            {
-                                var notification = new Notification(unread.length + " New Messages", {
-                                    body: body,
-                                    icon: "assets/outlook_linux_black.png"
-                                });
-                                notification.onclick = () => {
-                                    require('electron').ipcRenderer.send('show');
-                                };
-                            }
-                        }
-                    });
-                });
-            
-                observer.observe(unreadSpan, {childList: true});
-
-                // If the div containing reminders gets taller we probably got a new
-                // reminder, so force the window to the top.
-                let reminders = document.getElementsByClassName("_1BWPyOkN5zNVyfbTDKK1gM");
-                let height = 0;
-                let reminderObserver = new MutationObserver(mutations => {
-                    mutations.forEach(mutation => {
-                        if (reminders[0].clientHeight > height)
-                        {
-                            require('electron').ipcRenderer.send('show');
-                        }
-                        height = reminders[0].clientHeight;
-                    });
-                });
-
-                if (reminders.length) {
-                    reminderObserver.observe(reminders[0], { childList: true });
-                }
-
-            }, 10000);
-        `)
     }
 
     toggleWindow() {
@@ -126,23 +64,6 @@ class MailWindowController {
             this.win.hide()
         } else {
             this.show()
-        }
-    }
-
-    openInBrowser(e, url) {
-        console.log(url)
-        if (new RegExp(deeplinkUrls.join('|')).test(url)) {
-            // Default action - if the user wants to open mail in a new window - let them.
-        }
-        else if (new RegExp(outlookUrls.join('|')).test(url)) {
-            // Open calendar, contacts and tasks in the same window
-            e.preventDefault()
-            this.loadURL(url)
-        }
-        else {
-            // Send everything else to the browser
-            e.preventDefault()
-            shell.openExternal(url)
         }
     }
 
---
+++ src/inject/setup.js
@@ -0,0 +1,70 @@
+titleChange()
+configNotification()
+
+function configNotification() {
+  const userNameEle = document.querySelector('#O365_MeFlexPane_ButtonID')
+  if (!userNameEle) {
+    return setTimeout(configNotification, 1000)
+  }
+
+  try {
+    const userName = userNameEle.getAttribute('title');
+    const unreadSpan = document.querySelector('[aria-label="'+ userName +'"]  div div div span div div span');
+
+    const count = Number((unreadSpan.innerText || '').trim())
+
+    if (window.lastUnreadCount === count) {
+      return setTimeout(configNotification, 1000)
+    }
+
+    if (0 === count) {
+      window.lastUnreadCount = 0
+      return setTimeout(configNotification, 1000)
+    }
+
+    if (window.lastUnreadCount > count) {
+      window.lastUnreadCount = count
+      return setTimeout(configNotification, 1000)
+    }
+    
+    window.lastUnreadCount = !window.lastUnreadCount ? 0 : window.lastUnreadCount
+
+    const offset = count - window.lastUnreadCount
+
+    if (offset === 1) {
+      new Notification("New Messages", {
+        body: 'You have one unread email',
+        icon: "assets/outlook_linux_black.png"
+      });
+    } else {
+      new Notification("New Messages", {
+        body: 'You have '+ offset +' unread emails',
+        icon: "assets/outlook_linux_black.png"
+      });
+    }
+
+    window.lastUnreadCount = count
+    
+    return setTimeout(configNotification, 1000)
+  } catch (error) {
+    console.error('error', error);
+    return setTimeout(configNotification, 1000)
+  }
+}
+
+function titleChange () {
+  const oldTitle = document.querySelector('.o365cs-nav-gallatinLogo')
+  if (!oldTitle) {
+    return setTimeout(titleChange, 100)
+  }
+  const titleParent = oldTitle.parentNode
+  const newTitle = document.createElement('span')
+  newTitle.innerHTML = 'by 右领军大都督'
+  newTitle.className = oldTitle.className
+  newTitle.classList.remove('ms-Icon--gallatin')
+  newTitle.style.marginTop = '5px'
+
+  titleParent.removeChild(oldTitle)
+  titleParent.appendChild(newTitle)
+}
+
