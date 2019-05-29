0ce0fd2cd5291f6dba19c0954cfc9548033a77e4
Howard.Zuo <leftstick@qq.com>
2019-05-29 18:53:26 +0800

    refactor: chagne branding to Freelook

--- src/inject/setup.js
+++ src/inject/setup.js
@@ -1,5 +1,32 @@
 titleChange()
 configNotification()
+removeMainToolButton()
+changeBrandingText()
+
+function changeBrandingText() {
+  const texts = document.querySelectorAll('.o365cs-nav-brandingText')
+
+  if (!texts || !texts.length) {
+    return setTimeout(changeBrandingText, 500)
+  }
+
+  for (let i = 0; i < texts.length; i++) {
+    const text = texts[i]
+    if (text && text.innerText === 'Outlook') {
+      text.innerHTML = 'Freelook'
+    }
+  }
+}
+
+function removeMainToolButton() {
+  const btn = document.querySelector('#O365_MainLink_NavMenu')
+
+  if (!btn) {
+    return setTimeout(removeMainToolButton, 500)
+  }
+
+  btn.parentNode.removeChild(btn)
+}
 
 function configNotification() {
   const userNameEle = document.querySelector('#O365_MeFlexPane_ButtonID')
@@ -8,8 +35,8 @@ function configNotification() {
   }
 
   try {
-    const userName = userNameEle.getAttribute('title');
-    const unreadSpan = document.querySelector('[aria-label="'+ userName +'"]  div div div span div div span');
+    const userName = userNameEle.getAttribute('title')
+    const unreadSpan = document.querySelector('[aria-label="' + userName + '"]  div div div span div div span')
 
     const count = Number((unreadSpan.innerText || '').trim())
 
@@ -26,33 +53,33 @@ function configNotification() {
       window.lastUnreadCount = count
       return setTimeout(configNotification, 1000)
     }
-    
+
     window.lastUnreadCount = !window.lastUnreadCount ? 0 : window.lastUnreadCount
 
     const offset = count - window.lastUnreadCount
 
     if (offset === 1) {
-      new Notification("New Messages", {
+      new Notification('New Messages', {
         body: 'You have one unread email',
-        icon: "assets/outlook_linux_black.png"
-      });
+        icon: 'assets/outlook_linux_black.png'
+      })
     } else {
-      new Notification("New Messages", {
-        body: 'You have '+ offset +' unread emails',
-        icon: "assets/outlook_linux_black.png"
-      });
+      new Notification('New Messages', {
+        body: 'You have ' + offset + ' unread emails',
+        icon: 'assets/outlook_linux_black.png'
+      })
     }
 
     window.lastUnreadCount = count
-    
+
     return setTimeout(configNotification, 1000)
   } catch (error) {
-    console.error('error', error);
+    console.error('error', error)
     return setTimeout(configNotification, 1000)
   }
 }
 
-function titleChange () {
+function titleChange() {
   const oldTitle = document.querySelector('.o365cs-nav-gallatinLogo')
   if (!oldTitle) {
     return setTimeout(titleChange, 100)
@@ -67,4 +94,3 @@ function titleChange () {
   titleParent.removeChild(oldTitle)
   titleParent.appendChild(newTitle)
 }
-
