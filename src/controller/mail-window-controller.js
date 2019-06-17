c021c28a4266e7b9b3d41c2b83dc37d0c17f5b60
Howard.Zuo <leftstick@qq.com>
2019-06-17 13:54:01 +0800

    feat: external links should be opened individually

--- src/controller/mail-window-controller.js
+++ src/controller/mail-window-controller.js
@@ -5,72 +5,85 @@ const path = require('path')
 
 const outlookUrl = 'https://partner.outlook.cn/owa/'
 
+const INTERNAL_LINKS = ['https://partner.outlook.cn']
+
 class MailWindowController {
-    constructor() {
-        this.init()
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    // Get configurations.
+    const showWindowFrame = settings.get('showWindowFrame', true)
+
+    // Create the browser window.
+    this.win = new BrowserWindow({
+      x: 100,
+      y: 100,
+      width: 1400,
+      height: 900,
+      frame: showWindowFrame,
+      autoHideMenuBar: true,
+      show: false,
+      icon: path.join(__dirname, '../../assets/outlook_linux_black.png'),
+      webPreferences: {
+        webSecurity: false,
+        nodeIntegration: true,
+        allowRunningInsecureContent: true,
+        nativeWindowOpen: true,
+        preload: path.join(__dirname, '../inject/setup.js')
+      }
+    })
+
+    // and load the index.html of the app.
+    this.win.loadURL(outlookUrl)
+
+    // Show window handler
+    ipcMain.on('show', event => {
+      this.show()
+    })
+
+    //insert styles
+    this.win.webContents.on('dom-ready', () => {
+      this.win.webContents.insertCSS(CssInjector.main)
+      if (!showWindowFrame) this.win.webContents.insertCSS(CssInjector.noFrame)
+
+      this.win.show()
+    })
+
+    // Emitted when the window is closed.
+    this.win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      this.win = null
+    })
+
+    // Open the new window in external browser
+    this.win.webContents.on('new-window', this.openInBrowser)
+  }
+
+  toggleWindow() {
+    if (this.win.isFocused()) {
+      this.win.hide()
+    } else {
+      this.show()
     }
+  }
 
-    init() {
-        // Get configurations.
-        const showWindowFrame = settings.get('showWindowFrame', true)
-
-        // Create the browser window.
-        this.win = new BrowserWindow({
-            x: 100,
-            y: 100,
-            width: 1400,
-            height: 900,
-            frame: showWindowFrame,
-            autoHideMenuBar: true,
-            show: false,
-            icon: path.join(__dirname, '../../assets/outlook_linux_black.png'),
-            webPreferences: {
-                webSecurity: false,
-                nodeIntegration: true,
-                allowRunningInsecureContent: true,
-                nativeWindowOpen: true,
-                preload: path.join(__dirname, '../inject/setup.js')
-            }
-        })
-
-        // and load the index.html of the app.
-        this.win.loadURL(outlookUrl)
-
-        // Show window handler
-        ipcMain.on('show', (event) => {
-            this.show()
-        })
-
-        //insert styles
-        this.win.webContents.on('dom-ready', () => {
-            this.win.webContents.insertCSS(CssInjector.main)
-            if (!showWindowFrame) this.win.webContents.insertCSS(CssInjector.noFrame)
-
-            this.win.show()
-        })
-
-        // Emitted when the window is closed.
-        this.win.on('closed', () => {
-            // Dereference the window object, usually you would store windows
-            // in an array if your app supports multi windows, this is the time
-            // when you should delete the corresponding element.
-            this.win = null
-        })
-
+  openInBrowser(e, url) {
+    console.log('HELLO', url)
+    if (INTERNAL_LINKS.some(l => url.startsWith(l))) {
+      return
     }
+    e.preventDefault()
+    shell.openExternal(url)
+  }
 
-    toggleWindow() {
-        if (this.win.isFocused()) {
-            this.win.hide()
-        } else {
-            this.show()
-        }
-    }
-
-    show() {
-        this.win.show()
-        this.win.focus()
-    }
+  show() {
+    this.win.show()
+    this.win.focus()
+  }
 }
 
 module.exports = MailWindowController
